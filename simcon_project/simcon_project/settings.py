319ed44de463239f4e7115336a9ef941811a318f
Jeremy Carnahan <jcjerkie@gmail.com>
2020-11-23 19:45:57 -0800

    Moved templates and static to upper layer of simcon

--- simcon_project/simcon_project/settings.py
+++ simcon_project/simcon_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'simcon_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'simcon_project/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'simcon_project/static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'simcon_project/static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATIC_URL = '/static/'
