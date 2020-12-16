89bb7d4408c1c156db06f297cc502fa760704524
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2020-12-16 15:50:23 -0800

    added foreignkey to TemplateResponse

--- simcon_project/conversation_templates/models/template_node_response.py
+++ simcon_project/conversation_templates/models/template_node_response.py
@@ -6,4 +6,5 @@ class TemplateNodeResponse (models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     transcription = models.CharField(max_length=1000)
     template_node = models.ForeignKey('conversation_templates.TemplateNode',default=0, related_name='responses', on_delete=models.DO_NOTHING)
+    parent_template_response = models.ForeignKey('conversation_templates.TemplateResponse',default=0, related_name='node_responses', on_delete=models.CASCADE)
     position_in_sequence = models.IntegerField()
