108ede83d3aac6cd1cde82265cc9da1f542eb394
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2020-12-16 15:55:49 -0800

    update field name
    from file_name to label_name

--- simcon_project/users/models/subject_label.py
+++ simcon_project/users/models/subject_label.py
@@ -4,7 +4,7 @@ import uuid
 
 class SubjectLabel(models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
-    file_name = models.CharField(max_length=100)
+    label_name = models.CharField(max_length=100)
     students = models.ManyToManyField('users.Student', related_name='labels')
     researcher = models.ManyToManyField('users.Researcher', related_name='labels')
 
