e8222a16b2501d0c85483a85c2f209045d5e8dff
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2020-12-16 15:58:32 -0800

    changed relationship to Researcher
    Changed from ManyToManyField to ForeignKey because labels will not be shared between researchers.

--- simcon_project/users/models/subject_label.py
+++ simcon_project/users/models/subject_label.py
@@ -6,7 +6,7 @@ class SubjectLabel(models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     label_name = models.CharField(max_length=100)
     students = models.ManyToManyField('users.Student', related_name='labels')
-    researcher = models.ManyToManyField('users.Researcher', related_name='labels')
+    researcher = models.ForeignKey('users.Researcher', related_name='labels', default=0, on_delete=models.CASCADE)
 
     def get_file_name(self):
         return self.file_name
