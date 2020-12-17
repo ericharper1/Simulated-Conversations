0406d2db1b196529e77ae63bbe7d68c7cc8a2aa0
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2020-12-16 16:31:17 -0800

    Added docstring
    Simple docstring describes the class and its fields.

--- simcon_project/conversation_templates/models/conv_template.py
+++ simcon_project/conversation_templates/models/conv_template.py
@@ -4,6 +4,17 @@ import uuid
 
 
 class ConversationTemplate (models.Model):
+    """
+    Contains information related to an entire conversation template.
+    
+    Fields:
+    id: UUID to uniquely identify a template. Primary Key.
+    name: The name given to a template
+    description: The description of the template
+    creation_date: The date the template was created.
+    researcher: The researcher who created the template. Many to One (ForeignKey) relationship.
+        Deleting a researcher deletes any templates they created.
+    """
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=4000)
