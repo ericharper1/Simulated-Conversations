a2e8da18e5311f492346b6e82f9683fa4edfd623
Jeremy Carnahan <jcjerkie@gmail.com>
2020-12-18 15:21:33 -0800

    Working very basic response page

--- simcon_project/users/views/view_response.py
+++ simcon_project/users/views/view_response.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from conversation_templates.models import TemplateResponse, TemplateNodeResponse
 
 
-def ViewResponse(request, response=None):
+def ViewResponse(request, response=TemplateResponse.objects.get(id="e5f6dfd6-9a41-43e5-aadf-7747e4f33403")):
     if response is not None:
         nodes = []
-        for node in response.node_responses:
-            nodes.append(node)
+        num_nodes = TemplateNodeResponse.objects.filter(parent_template_response=response).count()
+        for i in range(1, num_nodes+1):
+            if TemplateNodeResponse.objects.get(parent_template_response=response, position_in_sequence=i):
+                nodes.append(TemplateNodeResponse.objects.get(parent_template_response=response, position_in_sequence=i))
+            else:
+                break
+
         return render(request, 'view_response.html', {'response': nodes})
     else:
         return render(request, 'invalid_response.html')
