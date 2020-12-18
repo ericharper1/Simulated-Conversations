36ce2b1dad11ffc1733618c5c59d48b7dd9b4712
Devin Hill <hilldev@pdex.edu>
2020-12-17 21:29:19 -0800

    Created a very simple student landing page that displays only the templates that have been assigned to them.

--- simcon_project/simcon_project/admin.py
+++ simcon_project/simcon_project/admin.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
-from django.apps import apps
+from django.contrib.auth.admin import UserAdmin
 from simcon_project.conversation_templates.models import *
 from simcon_project.users.models import *
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ('email', 'is_active', 'is_researcher', 'is_staff')
+    list_filter = ('email', 'is_active', 'is_researcher', 'is_staff')
+    fieldsets = (
+        (None, {
+            'fields': ('first_name', 'last_name', 'email', 'password')
+        }),
+        ('Permissions', {
+            'fields': ('is_active', 'is_researcher', 'is_staff')
+        }),
+    )
+    add_fieldsets = ((None, {
+        'classes': ('wide', ),
+        'fields': ('first_name', 'last_name', 'email', 'password1', 'password2', 'is_active',
+                   'is_researcher', 'is_staff')
+    }), )
+    search_fields = ('email', )
+    ordering = ('email', )
+
 
 admin.site.register(ConversationTemplate)
 admin.site.register(TemplateNode)
--- simcon_project/users/views/student_home.py
+++ simcon_project/users/views/student_home.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from users.models import *
+from conversation_templates.models import *
+
 
 def StudentView(request):
-    return render(request, 'student_view.html')
+    assigned_templates = Assignment.objects.filter(students=request.user.id)\
+        .values_list(
+            'conversation_templates__name',
+            'date_assigned',
+            'conversation_templates__template_responses__completion_date')\
+        .order_by('conversation_templates__template_responses__completion_date', 'date_assigned')
+    # must pass context as a dictionary
+    table_dict = {
+        'assigned_templates': assigned_templates
+    }
+    return render(request, 'student_view.html', table_dict)
