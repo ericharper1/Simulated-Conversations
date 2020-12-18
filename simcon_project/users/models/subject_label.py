348db13ab90aac1d5ae14977dfca1266118c6928
Devin Hill <hilldev@pdx.edu>
2020-12-18 14:56:25 -0800

    gave all UUIDfields a default of uuid.uuid4. had to import uuid for all models with UUIDfield

--- simcon_project/users/models/subject_label.py
+++ simcon_project/users/models/subject_label.py
@@ -4,7 +4,7 @@ import uuid
 
 class SubjectLabel(models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
-    label_name = models.CharField(max_length=100)
+    label_name = models.CharField(max_length=100, default=None)
     students = models.ManyToManyField('users.Student', related_name='labels')
     researcher = models.ForeignKey('users.Researcher', related_name='labels', default=0, on_delete=models.CASCADE)
 
