f57023e660a782ddcf9aa0adbf39eb4b8beedfd5
Jeremy Carnahan <jcjerkie@gmail.com>
2020-12-17 17:48:11 -0800

    Some basic work for response page, and a temporary invalid response page

--- simcon_project/users/views/view_response.py
+++ simcon_project/users/views/view_response.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
-def ViewResponse(request):
-    return render(request, 'view_response.html')
+
+def ViewResponse(request, response=None):
+    if response is not None:
+        nodes = []
+        for node in response.node_responses:
+            nodes.append(node)
+        return render(request, 'view_response.html', {'response': nodes})
+    else:
+        return render(request, 'invalid_response.html')
