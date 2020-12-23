6563b875ddb061801b44c383da0b0d9a9ef9156e
Eric Harper <harper28@pdx.edu>
2020-12-23 01:52:58 -0800

    Setup basic page routing. Created function to pass process metadata to template

--- simcon_project/conversation_templates/views/conversation.py
+++ simcon_project/conversation_templates/views/conversation.py
@@ -1,24 +1,39 @@
-from django.template.loader import get_template
-from django.template import Context
+from django.template import loader, RequestContext, Context
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+# Globals
 ct_templates_dir = 'conversation'
+user = get_user_model()
+
+
+# Returns data for Request process
+def proc_metadata(request):
+    return {
+        'user_email': request.user.__str__(),
+    }
+
+
+# Views
 def conversation_start(request):
-    ctx = {}
-    t_name = '{}/conversation_start.html'.format(ct_templates_dir)
-    t = get_template(t_name)
-    html = t.render(ctx)
-    return HttpResponse(html)
+    ctx = proc_metadata(request)
+    ctx_params = {}
+    ctx.update(ctx_params)
+    t = loader.get_template('{}/conversation_start.html'.format(ct_templates_dir))
+    return HttpResponse(t.render(ctx))
+
 
 def conversation_step(request):
-    ctx = {}
-    t_name = '{}/conversation_step.html'.format(ct_templates_dir)
-    t = get_template(t_name)
-    html = t.render(ctx)
-    return HttpResponse(html)
+    ctx = proc_metadata(request)
+    ctx_params = {}
+    ctx.update(ctx_params)
+    t = loader.get_template('{}/conversation_step.html'.format(ct_templates_dir))
+    return HttpResponse(t.render(ctx))
+
 
 def conversation_end(request):
+    ctx = proc_metadata(request)
     # for testing
     ct_list = [
         {
@@ -35,8 +50,7 @@ def conversation_end(request):
         }
     ]
 
-    ctx = {'ct_list': ct_list}
-    t_name = '{}/conversation_end.html'.format(ct_templates_dir)
-    t = get_template(t_name)
-    html = t.render(ctx)
-    return HttpResponse(html)
+    ctx_params = {'ct_list': ct_list}
+    ctx.update(ctx_params)
+    t = loader.get_template('{}/conversation_end.html'.format(ct_templates_dir))
+    return HttpResponse(t.render(ctx))
