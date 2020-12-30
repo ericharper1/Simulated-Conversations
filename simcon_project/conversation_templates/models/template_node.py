585ab684de74da4ba627bbf84c1d9fa060fb643b
Eric Harper <harper28@pdx.edu>
2020-12-29 21:02:03 -0800

    Added support for docstrings.

--- simcon_project/conversation_templates/models/template_node.py
+++ simcon_project/conversation_templates/models/template_node.py
@@ -4,6 +4,11 @@ import uuid
 
 
 class TemplateNode (models.Model):
+    """
+    Stores description and video for a node in a conversation, and has
+    fields to signify if node is the first or the last in a conversation.
+    Related to :model:`conversation_templates.ConversationTemplate`.
+    """
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     description = models.CharField(max_length=4000)
     video_url = EmbedVideoField(blank=False)
--- simcon_project/simcon_project/settings.py
+++ simcon_project/simcon_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
 
     # Third-party apps
     'embed_video',
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 ]
 
 ROOT_URLCONF = 'simcon_project.urls'
