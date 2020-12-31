268c9421634211dd5ffe2937233b6f578376163e
Devin Hill <hilldev@pdex.edu>
2020-12-31 15:34:46 -0800

    Added settings view and link to it from gear icon

--- simcon_project/simcon_project/urls.py
+++ simcon_project/simcon_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users.views import RedirectFromLogin, StudentView, ResearcherView
+from users.views import RedirectFromLogin, StudentView, ResearcherView, SettingsView
 from django.conf.urls import include
 from django.contrib.auth import views
 
@@ -27,4 +27,5 @@ urlpatterns = [
     path('redirect-from-login/', RedirectFromLogin, name="RedirectFromLogin"),
     path('student-view/', StudentView, name="StudentView"),
     path('researcher-view/', ResearcherView, name="ResearcherView"),
+    path('settings/', SettingsView, name="SettingsView"),
 ]
