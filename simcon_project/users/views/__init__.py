857a8d366fd5b274519f498fe598a423634b7e25
Devin Hill <hilldev@pdex.edu>
2020-12-31 16:28:54 -0800

    Made each template name link to a corresponding template start. Template start not fully built out yet.

--- simcon_project/users/views/__init__.py
+++ simcon_project/users/views/__init__.py
@@ -1,3 +1,5 @@
 from .redirect_from_login import RedirectFromLogin
 from .researcher_home import ResearcherView
 from .student_home import StudentView
+from .settings import SettingsView
+from. template_start import TemplateStartView
--- simcon_project/users/views/student_home.py
+++ simcon_project/users/views/student_home.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
-from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from users.models import *
 from conversation_templates.models import *
 
-
+@login_required
 def StudentView(request):
     assigned_templates = Assignment.objects.filter(students=request.user.id)\
         .values_list(
             'conversation_templates__name',
             'date_assigned',
-            'conversation_templates__template_responses__completion_date')\
+            'conversation_templates__template_responses__completion_date',
+            'conversation_templates__id')\
         .order_by('conversation_templates__template_responses__completion_date', 'date_assigned')
     assigned_templates_paginated = Paginator(assigned_templates, 10)
     page_number = request.GET.get('page')
