8b0e3ca1d548632b43a23d6cacd566ade4148bc3
Jeremy Carnahan <jcjerkie@gmail.com>
2021-01-06 12:02:52 -0800

    progressive form changing

--- simcon_project/users/views/view_response.py
+++ simcon_project/users/views/view_response.py
@@ -11,11 +11,30 @@ def ViewResponse(request, responseid="6b64f89a-176c-4b61-b9ac-29ede63e78b7"):
     response = get_object_or_404(TemplateResponse, pk=responseid)
 
     nodes = []
+    nodeform = []
     num_nodes = TemplateNodeResponse.objects.filter(parent_template_response=response).count()
     for i in range(1, num_nodes+1):
         if TemplateNodeResponse.objects.get(parent_template_response=response, position_in_sequence=i):
             nodes.append(TemplateNodeResponse.objects.get(parent_template_response=response,
                                                           position_in_sequence=i))
+            if request.method == 'POST':
+
+                # Create a form instance and populate it with data from the request (binding):
+                nodeform = UpdateFeedback(request.POST)
+
+                # Check if the form is valid:
+                if form.is_valid():
+                    # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+                    response.feedback = form.cleaned_data['feedback']
+                    response.save()
+
+                # redirect to a new URL:
+                return HttpResponseRedirect(reverse('ViewResponse'))
+
+            # If this is a GET (or any other method) create the default form.
+            else:
+                default_feedback = response.feedback
+                form = UpdateFeedback(initial={'feedback': default_feedback, })
         else:
             break
 
@@ -38,8 +57,7 @@ def ViewResponse(request, responseid="6b64f89a-176c-4b61-b9ac-29ede63e78b7"):
         default_feedback = response.feedback
         form = UpdateFeedback(initial={'feedback': default_feedback, })
 
-    form = UpdateFeedback(request.POST)
-    return render(request, 'view_response.html', {'response_nodes': nodes, 'response': response, 'form': form, 'nodeform': form})
+    return render(request, 'view_response.html', {'response_nodes': nodes, 'response': response, 'form': form, 'nodeform': nodeform})
 
 
 
