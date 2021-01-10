7d34357995ebc66e6a49ea4905c9c7b9231e113b
Xingjian <xingjian@pdx.edu>
2021-01-10 12:01:57 -0800

    rebase

--- simcon_project/conversation_templates/views/__init__.py
+++ simcon_project/conversation_templates/views/__init__.py
@@ -1,6 +1,6 @@
 from .conversation import *
 from .create_conversation_template_view import *
 from .edit_conversation_template_view import *
+from .create_assignment import *
 from .template_management import *
 from .template_responses_table import TemplateResponsesView
-from .view_response import *
