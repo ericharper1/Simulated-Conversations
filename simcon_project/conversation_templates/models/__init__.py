34a4d05124e2926fba5bfea4f26266534b7c10f0
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-11 15:26:47 -0800

    not a changed file.

--- simcon_project/conversation_templates/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .conv_template import ConversationTemplate 
-from .template_node_choice import TemplateNodeChoice
-from .template_node_response import TemplateNodeResponse
-from .template_node import TemplateNode
-from .template_response import TemplateResponse
-
