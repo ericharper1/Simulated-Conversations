1c1c23fcd0da62241cba9d34661aa93024e10fe2
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-11 17:27:57 -0800

    # Conflicts: #	simcon_project/conversation_templates/models/__init__.py #	simcon_project/simcon_project/admin.py #	simcon_project/simcon_project/settings.py #	simcon_project/users/admin.py #	simcon_project/users/models/__init__.py #	simcon_project/users/models/student.py #	simcon_project/users/models/subject_label.py

---
+++ simcon_project/conversation_templates/models/conv_template.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+import uuid
+
+
+class ConversationTemplate (models.Model):
+    """
+    Contains information related to an entire conversation template.
+    
+    Fields:
+    id: UUID to uniquely identify a template. Primary Key.
+    name: The name given to a template
+    description: The description of the template
+    creation_date: The date the template was created.
+    researcher: The researcher who created the template. Many to One (ForeignKey) relationship.
+        Deleting a researcher deletes any templates they created.
+    """
+    id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=4000)
+    creation_date = models.DateTimeField(default=timezone.now)
+    researcher = models.ForeignKey('users.Researcher', related_name='templates', default=0, on_delete=models.CASCADE)
+
+
---
+++ simcon_project/users/models/researcher.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .custom_user import CustomUser
+
+
+class Researcher(CustomUser):
+    pass
+
