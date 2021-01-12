74794d2d2829c44b0fada1f9d5fb8656771d7cba
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-11 16:12:39 -0800

    models are registered in users/admin.py, not here.

--- simcon_project/simcon_project/admin.py
+++ simcon_project/simcon_project/admin.py
@@ -1,43 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from simcon_project.conversation_templates.models import *
-from simcon_project.users.models import *
-from .forms import CustomUserCreationForm, CustomUserChangeForm
 
-
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ('email', 'is_active', 'is_researcher', 'is_staff')
-    list_filter = ('email', 'is_active', 'is_researcher', 'is_staff')
-    fieldsets = (
-        (None, {
-            'fields': ('first_name', 'last_name', 'email', 'password')
-        }),
-        ('Permissions', {
-            'fields': ('is_active', 'is_researcher', 'is_staff')
-        }),
-    )
-    add_fieldsets = ((None, {
-        'classes': ('wide', ),
-        'fields': ('first_name', 'last_name', 'email', 'password1', 'password2', 'is_active',
-                   'is_researcher', 'is_staff')
-    }), )
-    search_fields = ('email', )
-    ordering = ('email', )
-
-
-admin.site.register(ConversationTemplate)
-admin.site.register(TemplateNode)
-admin.site.register(TemplateNodeChoice)
-admin.site.register(TemplateResponse)
-admin.site.register(TemplateNodeResponse)
-admin.site.register(Assignment)
-admin.site.register(CustomUser)
-admin.site.register(CustomUserManager)
-admin.site.register(Researcher)
-admin.site.register(Student)
-admin.site.register(SubjectLabel)
-
+# Register your models here.
 
