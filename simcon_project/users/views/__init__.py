3ea12212c191ef9944ed66dff0b9858909fb6755
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-11 16:39:43 -0800

    not needed.

--- simcon_project/users/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from .redirect_from_login import RedirectFromLogin
-from .researcher_home import ResearcherView
-from .student_home import StudentView
-from .settings import SettingsView
-from .template_start import TemplateStartView
