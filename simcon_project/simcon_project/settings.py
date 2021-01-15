b0f1d9424bdf509837ee21af59f86fc76122173a
Jeremy Carnahan <jcjerkie@gmail.com>
2021-01-14 20:07:03 -0800

    audio fixed

--- simcon_project/simcon_project/settings.py
+++ simcon_project/simcon_project/settings.py
@@ -138,6 +138,6 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/redirect-from-login'
 
 MEDIAFILES_DIRS = [
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'static/media')
 ]
 MEDIA_URL = '/media/'
