b1552621b58b0d588273637b7ef369cf08a751f9
Jeremy Carnahan <jcjerkie@gmail.com>
2021-01-14 19:45:23 -0800

    David told me to

--- simcon_project/simcon_project/settings.py
+++ simcon_project/simcon_project/settings.py
@@ -130,10 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/redirect-from-login'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
 MEDIA_URL = '/media/'
