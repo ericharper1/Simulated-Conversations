3b94e6900bab91e46eb8f2a6ff257d226cc8d4be
Xingjian <xingjian@pdx.edu>
2021-01-22 19:48:58 -0800

    rebase

--- simcon_project/conversation_templates/views/create_assignment.py
+++ simcon_project/conversation_templates/views/create_assignment.py
@@ -8,6 +8,10 @@ from django.views.generic import TemplateView
 import json
 from django.core import serializers
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from apscheduler.schedulers.background import BackgroundScheduler
+from tzlocal import get_localzone
+
 
 class CreateAssignmentView(TemplateView):
     def get(self, request):
@@ -41,7 +45,25 @@ def decode(str):
         listTmp.append(t)
     return listTmp
 
+def sendMail(subject, msg, recipient, researcher):
+    send_mail(subject,msg,researcher,recipient,fail_silently=False)
+
+#Determine if data is empty.
+def isNull(data):
+    if len(data)!=0:
+        if len(data)==1:
+            if data[0]!='':
+                return False
+            return True
+        return False
+    return True
+
 def add_data(request):
+    #Error signs and error messages.
+    errMsg=''
+    success=0
+
+    #Get raw data
     data=request.POST
     name=data.get('name')
     date=data.get('date')
@@ -50,34 +72,70 @@ def add_data(request):
     templates=data.get('tempData')
     labels=data.get('labelData')
 
+    #Transfer string type list to list type
     students=decode(students)
     templates=decode(templates)
     labels=decode(labels)
 
+    #Save the data that can be directly assigned to assignment.
     assignment=Assignment()
     assignment.name=name
     assignment.date_assigned=date
     researcher=Researcher.objects.get(email=researcher)
     assignment.researcher=researcher
     assignment.save()
-    for stu in students:
-        stuTmp=Student.objects.get(email=stu)
-        assignment.students.add(stuTmp)
 
-    for temp in templates:
-        tempList=temp.split('--')
-        name=tempList[0]
-        date=tempList[1]
-        tempTmp=ConversationTemplate.objects.get(name=name,creation_date=date)
-        assignment.conversation_templates.add(tempTmp)
+    #Determine if students, labels, and templates are empty.
+    stuIsNull=isNull(students)
+    labelIsNull=isNull(labels)
+    tempIsNull=isNull(templates)
 
-    if len(labels)!=0:
-        if len(labels)==1 and labels[0]!='':
-            for label in labels:
-                labelTmp=SubjectLabel.objects.get(label_name=label)
-                assignment.subject_labels.add(labelTmp)
+    #Assign student information to assignment,
+    #and judge whether there is an error that both students and labels are empty.
+    if stuIsNull:
+        if labelIsNull:
+            success=1
+            errMsg='One of the students or labels must not be empty.'
+    else:
+        for stu in students:
+            stuTmp=Student.objects.get(email=stu)
+            assignment.students.add(stuTmp)
 
+    #Assign template information to assignment
+    if tempIsNull:
+        success=1
+        errMsg='Template must not be empty.'
+    else:
+        for temp in templates:
+            tempList=temp.split('--')
+            tempName=tempList[0]
+            tempDate=tempList[1]
+            tempTmp=ConversationTemplate.objects.get(name=tempName,creation_date=tempDate)
+            assignment.conversation_templates.add(tempTmp)
+
+    ##Assign label information to assignment
+    if not labelIsNull:
+        for label in labels:
+            labelTmp=SubjectLabel.objects.get(label_name=label)
+            assignment.subject_labels.add(labelTmp)
+
+    #Send email
+    job_defaults = { 'max_instances': 20 }
+    tz = get_localzone()
+    sched = BackgroundScheduler(timezone=tz, job_defaults=job_defaults)
+
+    subject='You received a new assignment.'
+    msg='You received this email because you have a new assignment: '+name+'. Please check the assignment page.'
+    schedId='Assignment--'+name
+    #run_date='2021-01-22 18:35:00'
+    sched.add_job(sendMail, trigger='date', id=schedId, run_date=date, args=(subject, msg, students, researcher))
+    sched.start()
+
+    # print(success)
+    # print(errMsg)
+    # print(repr(stuIsNull)+"  "+repr(labelIsNull)+"  "+repr(tempIsNull))
     return HttpResponse(json.dumps({
-        'success': 0,
+        'success': success,
+        'msg': errMsg,
     }))
 
