162f7bf43dc35d6239ce3347c203ed3d90981661
kaigoos <kaigustke@gmail.com>
2021-01-25 14:23:36 -0800

    Merged main and made a minor change to template_management.py

--- simcon_project/conversation_templates/views/template_management.py
+++ simcon_project/conversation_templates/views/template_management.py
@@ -29,6 +29,7 @@ class AllTemplateTable(tables.Table):
     Only used when all templates are displayed.
     """
     buttons = TemplateColumn(verbose_name='', template_name='template_management/buttons_template.html')
+    name = tables.columns.LinkColumn('management:folder_view', args=[A('pk')])
 
     class Meta:
         attrs = {'class': 'table table-sm', 'id': 'template-table'}
@@ -148,7 +149,6 @@ def remove_template(request, pk):
     This does not delete the template.
     """
     if request.method == 'POST':
-
         previous_url = request.META.get('HTTP_REFERER')
         folder_pk = re.findall(r"/folder/([A-Za-z0-9\-]+)", previous_url)[0]
         template = get_object_or_404(ConversationTemplate, pk=pk)
