36641d3b195a37f50737ecd9519c1506201cb244
kaigoos <kaigustke@gmail.com>
2021-01-26 14:37:50 -0800

    Added a dropdown to the page so user can navigate to other templates without going back to template management page

--- simcon_project/conversation_templates/views/template_responses_table.py
+++ simcon_project/conversation_templates/views/template_responses_table.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django_tables2 import tables, RequestConfig, SingleTableView
 from django_tables2.export.views import TableExport
-from conversation_templates.models import ConversationTemplate, TemplateFolder, TemplateResponse
+from conversation_templates.models import ConversationTemplate, TemplateResponse
+from conversation_templates.forms import SelectTemplateForm
 
 
 class ResponseTable(tables.Table):
@@ -16,9 +18,16 @@ class ResponseTable(tables.Table):
 class TemplateResponsesView(SingleTableView):
     model = TemplateResponse
     table_class = ResponseTable
-    template_name = "template_all_response_view.html"
+    template_name = "template_all_responses_view.html"
 
     def get(self, request, pk):
+        """
+        On get request render a custom table based on ResponseTable:
+        Rows: all template responses for the conversation template
+        Columns: student.first_name, student.last_name, template_response.completion_date,
+                 and template_node_responses with template_node.description as headers (this is
+                 dynamically created since each template has different number of nodes")
+        """
         template = ConversationTemplate.objects.get(pk=pk)
         descriptions = []  # Description of template node used for column header
         extra_columns = []  # List of tuples of description and column object to pass to table
@@ -28,6 +37,7 @@ class TemplateResponsesView(SingleTableView):
             descriptions.append(node.description)
             extra_columns.append((node.description, tables.columns.Column()))
 
+        # Populate the table with data for each response
         for response in template.template_responses.all().order_by('-completion_date'):
             column_data = {
                 "first": response.student.first_name,
@@ -45,11 +55,21 @@ class TemplateResponsesView(SingleTableView):
         export_format = request.GET.get("_export", None)
         context = {
             "table": response_table,
+            "form": SelectTemplateForm(request=request, initial=template.name)
         }
+        if not template.template_nodes.all():
+            context.update({"empty": True})
 
+        # Code needed to export table as .xls
         if TableExport.is_valid_format(export_format):
             exporter = TableExport(export_format, response_table)
             file_name = f"{template.name}.{format(export_format)}"
             return exporter.response(file_name)
 
         return render(request, self.template_name, context)
+
+
+    def post(self, request, pk):
+        # Note: keep pk even if it isn't used since the url requires it.
+        # Route to selected template_responses_table from the choicefield
+        return redirect(reverse('TemplateResponsesView', args=[request.POST['templates']]))
