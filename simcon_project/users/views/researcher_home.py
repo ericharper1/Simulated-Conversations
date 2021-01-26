4130d14ae8a6993e6d79d59753de4d498985ece8
Katie Rosas <54516136+rosasck@users.noreply.github.com>
2021-01-25 21:57:05 -0800

    search for responses

--- simcon_project/users/views/researcher_home.py
+++ simcon_project/users/views/researcher_home.py
@@ -10,6 +10,20 @@ def ResearcherView(request):
     if request.method == "POST":
         items_to_delete = request.POST.getlist('delete_items')
         responseTable.filter(pk__in=items_to_delete).delete()
+    if 'searchParam' in request.GET and request.GET['searchParam']:
+        searchParam = request.GET['searchParam']
+        searchResponses = []
+        search1 = TemplateResponse.objects.filter(
+            student__email__icontains=searchParam)
+        searchResponses.append(search1)
+        search2 = TemplateResponse.objects.filter(
+            template__name__icontains=searchParam)
+        searchResponses.append(search2)
+        search3 = TemplateResponse.objects.filter(
+            assignment__subject_labels__label_name__icontains=searchParam)
+        searchResponses.append(search3)
+        return render(request, 'researcher_view.html',
+                      {'searchResponses': searchResponses})
     paginator = Paginator(responseTable, 10)
     pageNumber = request.GET.get('page')
     pageObject = paginator.get_page(pageNumber)
