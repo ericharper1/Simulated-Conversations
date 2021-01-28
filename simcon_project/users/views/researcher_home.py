ba4e0228d6854b58b99c7b9fe8b9ddc415a3d51e
Xingjian <xingjian@pdx.edu>
2021-01-28 10:02:27 -0800

    rebase

--- simcon_project/users/views/researcher_home.py
+++ simcon_project/users/views/researcher_home.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from conversation_templates.models import TemplateResponse
 from django.contrib.auth.decorators import user_passes_test
 from django.core.paginator import Paginator
