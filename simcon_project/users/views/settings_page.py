64cb4022b1e7cb90122160a87b4c7e8f7041a257
Xingjian <xingjian@pdx.edu>
2021-01-28 10:02:27 -0800

    rebase

--- simcon_project/users/views/settings_page.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-
-@login_required
-def SettingsView(request):
-    return render(request, 'settings_view.html')
