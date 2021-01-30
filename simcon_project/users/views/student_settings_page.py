7c486b5f9e1b955b32cab18982337c7398045a97
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-29 19:30:59 -0800

    removed unnecessary import of login_required

--- simcon_project/users/views/student_settings_page.py
+++ simcon_project/users/views/student_settings_page.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import PasswordChangeForm
