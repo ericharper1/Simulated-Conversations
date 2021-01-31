348e926d582f6f2763e162b4038c2cd3c4c4e7fb
Devin Hill <57743179+BroughtMyPopcorn@users.noreply.github.com>
2021-01-30 18:27:46 -0800

    fixed RedirectFromLogin

--- simcon_project/users/views/redirect_from_login.py
+++ simcon_project/users/views/redirect_from_login.py
@@ -1,14 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
 
 @login_required
 def RedirectFromLogin(request):
     user = get_user_model()
     if user.get_is_researcher(request.user):
-        return HttpResponseRedirect('ResearcherView')
+        return HttpResponseRedirect(reverse('ResearcherView'))
     else:
-        return HttpResponseRedirect('StudentView')
+        return HttpResponseRedirect(reverse('StudentView'))
 
 
 
