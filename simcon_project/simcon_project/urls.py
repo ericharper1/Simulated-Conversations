11aa9b79bae37b4b0a62a981629aad36977e7eb4
Xingjian <xingjian@pdx.edu>
2021-02-03 20:17:14 -0800

    rebase

--- simcon_project/simcon_project/urls.py
+++ simcon_project/simcon_project/urls.py
@@ -55,7 +55,7 @@ urlpatterns = [
     path('researcher/response/update/<uuid:pk>/', update_overall_response_feedback, name='update-overall-response-feedback'),
     path('researcher/response/updatenode/<uuid:pk>/', update_node_response_feedback, name='update-node-response-feedback'),
     path('researcher/response/updatetranscription/<uuid:pk>/', update_node_transcription, name='update-node-transcription'),
-    path('researcher/create-assignment/',include('conversation_templates.urls.create_assignment_urls'), name="CreateAssignmentView"),
+    path('researcher/create-assignment/',include('users.create_assignment_urls'), name="create-assignment-view"),
 ]
 
 if settings.DEBUG:
