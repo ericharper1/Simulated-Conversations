59d0c1f59eace0e7aa8ca13858d95030f87e3a1f
Eric Harper <harper28@pdx.edu>
2021-02-03 17:17:51 -0800

    Forgot to make that change.

--- simcon_project/conversation_templates/models/template_node_response.py
+++ simcon_project/conversation_templates/models/template_node_response.py
@@ -19,9 +19,9 @@ class TemplateNodeResponse(models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     transcription = models.CharField(max_length=1000, null=True, blank=True, default=None)
     template_node = models.ForeignKey('conversation_templates.TemplateNode', default=0, null=True, blank=True, related_name='responses', on_delete=models.DO_NOTHING)
-    parent_template_response = models.ForeignKey('conversation_templates.TemplateResponse', default=0, null=True, blank=True, related_name='node_responses', on_delete=models.CASCADE)
+    parent_template_response = models.ForeignKey('conversation_templates.TemplateResponse', default=0, related_name='node_responses', on_delete=models.CASCADE)
     selected_choice = models.ForeignKey('conversation_templates.TemplateNodeChoice', default=0, null=True, blank=True, related_name='node_response', on_delete=models.DO_NOTHING)
-    position_in_sequence = models.IntegerField(null=True, blank=True)
+    position_in_sequence = models.IntegerField()
     feedback = models.CharField(max_length=1000, default=None, null=True, blank=True)
     audio_response = models.FileField(upload_to='audio/%Y/%m/%d', default=None)
 
