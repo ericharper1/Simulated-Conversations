c84741c059709e52cdb0b895e37580ab0bd39d00
David Likhovodov <likhovodov@gmail.com>
2021-02-04 18:33:31 -0800

    Finished patching create conversation page

--- simcon_project/conversation_templates/urls/templates_urls.py
+++ simcon_project/conversation_templates/urls/templates_urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('folder/new/', FolderCreateView.as_view(), name='create-folder'),
     path('folder/delete/<uuid:pk>/', FolderDeleteView.as_view(), name='delete-folder'),
     path('folder/edit/<uuid:pk>/', FolderEditView.as_view(), name='edit-folder'),
-    path('templates/delete/<uuid:pk>/', TemplateDeleteView.as_view(), name="delete-template"),
+    path('delete/<uuid:pk>/', TemplateDeleteView.as_view(), name="delete-template"),
     path('folder/remove/<uuid:pk>/', remove_template, name="remove-template"),
-    path('templates/new', create_conversation_template_view, name="create-conversation-template-view"),
+    path('new/', create_conversation_template_view, name="create-conversation-template-view"),
 ]
