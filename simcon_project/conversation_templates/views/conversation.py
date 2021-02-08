0db73768e9f7a9602f723ad39e5e2874c9820685
Jeremy Carnahan <jcjerkie@gmail.com>
2021-02-07 21:49:09 -0800

    Consistency changes

--- simcon_project/conversation_templates/views/conversation.py
+++ simcon_project/conversation_templates/views/conversation.py
@@ -161,7 +161,7 @@ def conversation_end(request, ct_response_id):
             response.transcription = request.POST.get(str(response.id), '')
             response.save()
         # The key has to be 0, I have no clue why, just don't touch it
-        ct_response.self_rating = request.POST.get('0', 3)
+        ct_response.self_rating = request.POST.get('0', 0)
         ct_response.save()
         if ct_response.completion_date is None:
             ct_response.completion_date = timezone.now()
