d4c26a657548541977036d762d95943ff9f7108d
Xingjian <59945588+xingjianW@users.noreply.github.com>
2021-02-13 23:02:32 -0800

    Made some fixes to the code based on the comments of the PR.

--- simcon_project/users/create_assignment_urls.py
+++ simcon_project/users/create_assignment_urls.py
@@ -4,5 +4,5 @@ from users.views import *
 app_name = 'assignments'
 urlpatterns = [
     path('', CreateAssignmentView.as_view(), name="create-assignment"),
-    path('add_data/', add_data, name="add-data"),
+    path('add_assignment/', add_assignment, name="add-assignment"),
 ]
