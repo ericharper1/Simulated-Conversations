bf96363af8475e7fb3e0334dc9eccadd07de1999
Admin <hilldev@pdx.edu>
2021-02-16 21:04:02 -0800

    updated email model to delete along with their assignment.

--- simcon_project/users/models/email.py
+++ simcon_project/users/models/email.py
@@ -6,4 +6,4 @@ class Email(models.Model):
     id = models.UUIDField(unique=True, editable=False, primary_key=True, default=uuid.uuid4)
     subject = models.CharField(max_length=100, default=None)
     message = models.CharField(max_length=500, default=None)
-    assignment = models.OneToOneField('users.Assignment', related_name='email', on_delete=models.DO_NOTHING)
+    assignment = models.OneToOneField('users.Assignment', related_name='email', on_delete=models.CASCADE)
