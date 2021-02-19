19791e705fc47fbdf1eee581d1cdd746cd12195f
Admin <hilldev@pdx.edu>
2021-02-19 13:00:08 -0800

    continuing...

--- simcon_project/users/views/redirect_from_login.py
+++ simcon_project/users/views/redirect_from_login.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import user_passes_test
 from django.urls import reverse
+import django.utils.timezone as timezone
 
 
 def is_authenticated(user):
@@ -11,6 +12,7 @@ def is_authenticated(user):
 @user_passes_test(is_authenticated)
 def redirect_from_login(request):
     user = get_user_model()
+    timezone.activate(timezone.get_current_timezone_name())
     if user.get_is_researcher(request.user):
         return HttpResponseRedirect(reverse('researcher-view'))
     else:
